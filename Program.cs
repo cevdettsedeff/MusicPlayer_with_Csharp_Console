6d90d55f8729481f8f4d812f3ae2467a992fb75e
Cevdet Sedef <cevdettsedeff@gmail.com>
2022-10-11 00:57:43 +0300

    The project was created.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+namespace MusicPlayer_with_Csharp_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SoundPlayer voice = new SoundPlayer();
+            string adress;
+
+            adress = @"C:\Users\Cevsev\Desktop\Wavfile.wav";
+
+            Console.WriteLine("----------------");
+
+            voice.SoundLocation= adress;
+
+            voice.Play();
+            Console.WriteLine("\nMusic is playing...");
+            Console.WriteLine("\nIf you want to stop to music, please press the enter!");
+            Console.WriteLine("\n----------------");
+
+            Console.ReadLine();
+
+            voice.Stop();
+
+            Console.WriteLine("\nMusic is stopped...");
+            Console.ReadLine();
+
+        }
+    }
+}
